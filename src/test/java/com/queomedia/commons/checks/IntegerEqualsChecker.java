a8cb49e72f584ab7df4fe2c399502aa6e817a8a4
Ralph Engelmann <r.engelmann@queo-group.com>
2010-07-21 09:50:01 +0000

    Uniqe Element eingebaut

---
+++ src/test/java/com/queomedia/commons/checks/IntegerEqualsChecker.java
@@ -0,0 +1,29 @@
+package com.queomedia.commons.checks;
+
+import com.queomedia.commons.equals.EqualsChecker;
+
+/**
+ * Check if two integers are equals. 
+ *
+ *
+ */
+public final class IntegerEqualsChecker implements EqualsChecker<Integer, Integer> {
+
+    /** The only one instance */
+    public static final IntegerEqualsChecker INSTANCHE = new IntegerEqualsChecker();
+    
+    /** Use {@link #INSTANCHE} instead. */
+    private IntegerEqualsChecker() {
+        super();
+    }
+    
+    @Override
+    public boolean equals(Integer objectT, Integer objectK) {
+        if (objectT == null) {
+            return objectK == null;
+        } else {
+            return objectT.equals(objectK);
+        }
+    }
+
+}
